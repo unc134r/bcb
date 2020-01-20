f1647817dcc3c5b51586e21967c9845665cf4cf7
mirsha <mircea@atelieru.ro>
2020-01-20 15:45:02 +0200

    simplifying things
    refactored code to ES6 standards;
    changed recursive function to forEach;
    deleted redundant if

--- content.js
+++ content.js
@@ -1,19 +1,12 @@
-Array.from(document.getElementsByClassName("r")).forEach(
-    function(element, index, array) {
-	    var who = document.getElementsByClassName("r")[index];
-	    console.log(who.firstElementChild.childElementCount );
-	    if (who.firstElementChild.childElementCount == 3){
-	        var parent_r = who.firstElementChild; //the A tag
-			var f_child = parent_r.children[0];
-			var s_child = parent_r.children[1];
-			var to_save_1 = f_child.cloneNode(true); 
-			var to_save_2 = s_child.cloneNode(true); 
+document.querySelectorAll(".r").forEach(
+    index=>{
+	        const parent_r = index.firstElementChild; //the A tag
+			const f_child = parent_r.children[0];
+			const s_child = parent_r.children[1];
+			const to_save_1 = f_child.cloneNode(true); 
+			const to_save_2 = s_child.cloneNode(true); 
 			parent_r.appendChild(to_save_2);
 			parent_r.appendChild(to_save_1);
-			
 			parent_r.removeChild(parent_r.firstElementChild);	
 			parent_r.removeChild(parent_r.firstElementChild);	
-			}
-	
-	    }
-);
+});
